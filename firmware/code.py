372c432a1ce561f28ca7bcb7bc7c2da88bd671b0
Kevin <wldd2211@gmail.com>
2026-02-24 18:00:19 +0530

    Add modifier layer to firmware

--- firmware/code.py
+++ firmware/code.py
@@ -1,30 +1,40 @@
 import board
-
-print("Starting")
-
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
+from kmk.modules.layers import Layers
 
 keyboard = KMKKeyboard()
+layers_ext = Layers()
 
+keyboard.modules.append(layers_ext)
+
+# Define hardware pins
 keyboard.col_pins = (board.D4, board.D5, board.D6)
 keyboard.row_pins = (board.D0, board.D1, board.D2, board.D3)
 keyboard.diode_orientation = DiodeOrientation.ROW2COL
 
+# Keymap setup:
+# Layer 0 = default
+# Layer 1 = F13...F23 outputs if the modifier key is held
+
 keyboard.keymap = [
+    # LAYER 0 – default
     [
         KC.N7, KC.N8, KC.N9,
         KC.N4, KC.N5, KC.N6,
         KC.N1, KC.N2, KC.N3,
-        KC.A,  KC.N0, KC.B,
+        KC.MO(1), KC.N0, KC.BSPC,
+    ],
+
+    # LAYER 1 – F13–F23 when modifier held
+    [
+        KC.F13, KC.F14, KC.F15,
+        KC.F16, KC.F17, KC.F18,
+        KC.F19, KC.F20, KC.F21,
+        KC.TRNS, KC.F22, KC.F23,
     ]
 ]
 
-# keyboard.rgb.set_rgb(255, 255, 255, 0)
-# keyboard.rgb.set_rgb(255, 255, 255, 1)
-# keyboard.rgb.set_rgb(255, 255, 255, 2)
-# keyboard.rgb.show()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     keyboard.go()
